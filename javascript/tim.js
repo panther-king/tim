9ed265be76221e41d644fb2e989129d442449583
Taro Yamashita <taro@panther-king.com>
2014-10-25 20:23:04 +0900

    Implements timer functions

--- javascript/tim.js
+++ javascript/tim.js
@@ -12,8 +12,30 @@
       down: function(context) {
         context.counter -= 60;
       },
+      minute: function(value) {
+        if (value === 60) {
+          return 1;
+        } else if (value < 60) {
+          return 0;
+        } else {
+          return Math.floor(value / 60);
+        }
+      },
+      render: function(value) {
+        if (value === 0) {
+          return '00';
+        } else if (value < 10) {
+          return '0' + value.toString();
+        } else {
+          return value.toString();
+        }
+      },
       reset: function(context) {
         context.counter = 0;
+      },
+      second: function(value) {
+        var minute = Math.floor(value / 60);
+        return value - (minute * 60);
       },
       start: function(context) {
         context.timer = setInterval(function() {
@@ -30,6 +52,7 @@
   });
 
   vm.$watch('counter', function(value, mutation) {
-    console.log(value);
+    this.minutes = this.render(this.minute(value));
+    this.seconds = this.render(this.second(value));
   });
 })();
